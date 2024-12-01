6f2d8de4065c858fa922a4434aa333ac484ef657
dsoman24 <dsoman24@gmail.com>
2024-12-01 10:51:41 -0500

    implement mAP

--- models/recommender.py
+++ models/recommender.py
@@ -9,6 +9,7 @@
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 
 class Recommender:
@@ -37,36 +38,33 @@ class Recommender:
         """
         user_data = self.user_data_df[self.user_data_df['user_id'] == user]
         user_data = user_data.merge(self.predictions_df, on='song_id')
-        weighted_prediction = None
-        total_count = 0
-        for index, row in user_data.iterrows():
-            prediction = np.array(row['prediction'])
-            count = row['play_count']
-            if total_count == 0:
-                weighted_prediction = prediction * count
-            else:
-                weighted_prediction += prediction * count
-            total_count += count
-        if total_count == 0:
+        if user_data.empty:
             return None
-        return weighted_prediction / total_count
+        counts = user_data['play_count'].values.reshape(-1, 1)
+        predictions = np.stack(user_data['prediction'].values)
+        weighted_predictions = predictions * counts
+        total_count = counts.sum()
+        return weighted_predictions.sum(axis=0) / total_count
 
-    def recommend_songs(self, user, n=10):
+    def recommend(self, user, n=10, omit_listened_to=False):
         """
         Returns a table of n song ids to recommend to the user.
         """
-        not_listened_to = self._songs_not_listened_to(user)
-        predictions_not_listened_to = self.predictions_df[self.predictions_df['song_id'].isin(not_listened_to)]
+        if omit_listened_to:
+            not_listened_to = self._songs_not_listened_to(user)
+            predictions = self.predictions_df[self.predictions_df['song_id'].isin(not_listened_to)]
+        else:
+            predictions = self.predictions_df
         fingerprint = self._generate_fingerprint(user)
         if fingerprint is None:
             self._print_debug(f"No fingerprint available for user {user}.")
             return None
         self._print_debug(f"Fingerprint for user {user}: {fingerprint}")
-        predictions_not_listened_to['similarity'] = predictions_not_listened_to['prediction'].apply(lambda x: np.dot(np.array(x), fingerprint))
-        predictions_not_listened_to = predictions_not_listened_to.sort_values('similarity', ascending=False)
-        return predictions_not_listened_to.head(n)
+        predictions['similarity'] = predictions['prediction'].apply(lambda x: np.dot(np.array(x), fingerprint))
+        predictions = predictions.sort_values('similarity', ascending=False)
+        return predictions.head(n)
 
-    def recommend_users(self, user, n=10):
+    def similar_users(self, user, n=10):
         """
         Returns a table of n user ids to recommend to the user.
         """
@@ -90,3 +88,36 @@ class Recommender:
         similarities_df = pd.DataFrame(similarities)
         similarities_df = similarities_df.sort_values('similarity', ascending=False).head(n)
         return similarities_df
+
+
+class RecommenderEvaluator:
+    """
+    This class evaluates a recommender system using baseline metrics:
+    - Mean Average Precision (mAP)
+    - Area under ROC curve (AUC)
+    """
+
+    def __init__(self, recommender):
+        self.recommender = recommender
+
+    def evaluate(self):
+        mAP = self._mean_average_precision()
+        return mAP
+
+    def _mean_average_precision(self, n=500):
+        users = self.recommender.user_data_df['user_id'].unique()
+        average_precisions = []
+        for user in tqdm(users, desc=f"Computing mAP with top {n} recommendations per user"):
+            recommended = self.recommender.recommend(user, n, omit_listened_to=False)['song_id'].tolist()
+            relevant = set(self.recommender.user_data_df[self.recommender.user_data_df['user_id'] == user]['song_id'])
+            if not relevant:
+                continue
+            score = 0.0
+            num_hits = 0
+            for i, song_id in enumerate(recommended):
+                if song_id in relevant:
+                    num_hits += 1
+                    score += num_hits / (i + 1)
+            if num_hits > 0:
+                average_precisions.append(score / min(len(relevant), 10))
+        return np.mean(average_precisions) if average_precisions else 0.0
